5fadc5001f7c9b2f6f9156dcbfc7a5483083d84a
Taliah Wharton <taliahwh23@yahoo.com>
2023-01-11 11:23:04 -0500

    sqaure sorted arrays

---
+++ arrays/squareSortedArr.js
@@ -0,0 +1,26 @@
+const squareSortedArr = (nums) => {
+  let start = 0;
+  let end = nums.length - 1;
+  let index = end;
+  const newSortedArr = [];
+
+  while (start <= end) {
+    const left = nums[start] * nums[start];
+    const right = nums[end] * nums[end];
+
+    if (left > right) {
+      newSortedArr[index] = left;
+      start++;
+    } else {
+      // right > left
+      newSortedArr[index] = right;
+      end--;
+    }
+    index--;
+  }
+  return newSortedArr;
+};
+
+const nums = [-4, -1, 0, 3, 10];
+const sorted = squareSortedArr(nums);
+console.log(sorted);
