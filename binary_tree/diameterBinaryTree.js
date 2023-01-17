c524ea5a79b3cb7d354f1049a5a15f0de48e2766
Taliah Wharton <taliahwh23@yahoo.com>
2023-01-17 10:59:31 -0500

    diameter of binary tree

--- binary_tree/diameterBinaryTree.js
+++ binary_tree/diameterBinaryTree.js
@@ -2,11 +2,23 @@
  * The diamater of a binary tree is the LENGTH OF LONGEST PATH BETWEEN TWO NODES.
  * The nodes do not have to pass through the root.
  * Length of a path is the represented by EDGES, not nodes.
+ *
+ * If we were simply finding the diameter of a binary tree through the root, we could
+ * add togather the longest path from either side.
+ * Since the maxDiameter of the tree could be on any node that isn't the root, we'll have to
+ * maintain a MAX var that keeps track of the largest path we've seen between the left + right
+ * paths.
+ *
+ * This soltion is a spin off of a longestPath algorithm
+ *
+ * Time: O(n)
+ * Space O(n) -> in the worst case, we will traverse the height of the tree
+ * (if the tree was balanced, the Space Complexity would be O(logn))
  */
-const diameterBinaryTree = (root) => {
+const diameterOfBinaryTree = (root) => {
   if (!root) return 0;
 
-  let max = 0;
+  let maxDiameter = 0;
 
   const dfs = (node) => {
     if (!node) return 0;
@@ -14,11 +26,11 @@ const diameterBinaryTree = (root) => {
     const left = dfs(node.left);
     const right = dfs(node.right);
 
-    max = Math.max(max, left + right);
+    maxDiameter = Math.max(maxDiameter, left + right);
 
     // return max of either left or right side, + 1 to account for current level
     return Math.max(left, right) + 1;
   };
   dfs(root);
-  return max;
+  return maxDiameter;
 };
