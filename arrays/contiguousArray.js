ff7056b5633e5292c161f6a5cb2e707951985417
Taliah Wharton <taliahwh23@yahoo.com>
2023-02-07 16:08:08 -0500

    hashmap, max value logic

--- arrays/contiguousArray.js
+++ arrays/contiguousArray.js
@@ -1,49 +1,52 @@
+/**
+ * We know that the integers can only be 0's or 1's, therefor if we think of 0's as -1's,
+ * any sum of a contiguous array with an equal number of 0's and 1's will always be equal to 0.
+ *
+ * The first step will be to iterate though the nums input array and for each iteration:
+ * update the current sum
+ *
+ * Before storing the sum and its index and value into a hashmap, we'll want to check snd see
+ * if the current sum has been stored in the hashmap before -> if it has, that means we have
+ * located a contiguous array that meets the parameters (equal 0's and 1's)
+ *
+ * The length of the contiguous array will be the current index - index in hashmap with matching sum
+ * update maxLength
+ *
+ * return maxLength, if 0 -> return -1
+ *
+ *
+ */
+
 const findMaxLength = (nums) => {
-  // init a hashmap to count binary frequency in window
-  let count = {};
-  let max = 0;
+  const prevSum = {}; // { sum : index }
+  let sum = 0;
+  let maxLength = 0;
 
-  // init windowStart var that will be used to eventually shrink the window
-  let windowStart = 0;
+  for (let i = 0; i < nums.length; i++) {
+    // update sum, if nums[i] = 0, use -1 as its value
+    const binary = nums[i];
+    binary === 0 ? (sum += -1) : (sum += 1);
 
-  // iterate through input arr
-  for (let windowEnd = 0; windowEnd < nums.length; windowEnd++) {
-    // add current num to hashmap count
-    const num = nums[windowEnd];
-
-    if (!(num in count)) count[num] = 0;
-    count[num]++;
-
-    // extend the window in multiples of two since we need for the 0s and 1s to be equal
-    let windowLength = windowEnd - windowStart + 1;
-    console.log(
-      'windowLength % 2 === 0:',
-      windowLength % 2 === 0,
-      'count',
-      count
-    );
-
-    while (windowLength % 2 === 0) {
-      // check if 0s and 1s are equal, if they are -> update max
-      if (count[0] === count[1]) {
-        max = Math.max(max, windowLength);
-        windowEnd++;
-      } else {
-        // slide the window forward by removing the first element
-        // remember to update the hashmap count as well
-        count[num]--;
-        windowStart++;
-      }
+    // check if sum has appeared in prevSum, if it has we've found a contiguous array with
+    // equal 0's and 1's, so we'll need to find the length by subtracting the index at that
+    // key value pair from the current index
+    if (sum in prevSum) {
+      const length = i - prevSum[sum];
+      maxLength = Math.max(maxLength, length);
+    } else {
+      // store current sum and index into prevSum for future reference
+      prevSum[sum] = i;
     }
-    console.log('max:', max);
-    console.log('-----------------------------------');
+    if (sum === 0) maxLength = Math.max(maxLength, i + 1);
   }
-
-  return max === 0 ? -1 : max;
+  return maxLength;
 };
 
-// console.log(findMaxLength([0, 0])); // -> -1
-// console.log(findMaxLength([0, 0, 1])); // -> 2
-// console.log(findMaxLength([0, 1])); // -> 2
+console.log(findMaxLength([0, 0])); // -> -1
+console.log(findMaxLength([0, 0, 1])); // -> 2
+console.log(findMaxLength([0, 1])); // -> 2
 console.log(findMaxLength([0, 1, 0])); // -> 2
-// console.log(findMaxLength([0, 1, 0, 1, 1, 0, 0])); // -> 6
+console.log(findMaxLength([0, 1, 0, 1, 1, 0, 0])); // -> 6
+console.log(findMaxLength([1, 1, 1, 1, 1, 1, 1, 1])); // -> 0
+
+// 0, 1, 0, 1, 1, 1, 0, 1, 0
