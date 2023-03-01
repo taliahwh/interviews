c33413aca30a46999e466519011d852dace5375d
Taliah Wharton <taliahwh23@yahoo.com>
2023-03-01 10:31:01 -0500

    refactored solution using stack

--- stack/dailyTemperatures.js
+++ stack/dailyTemperatures.js
@@ -1,3 +1,35 @@
+// Time: O(n)
+// Space: O(n)
+const dailyTemperatures = (temperatures) => {
+  const answer = new Array(temperatures.length).fill(0);
+  const stack = []; // [futureDay, index]
+
+  // iterate through array backwards
+  for (let i = temperatures.length - 1; i >= 0; i--) {
+    while (stack.length) {
+      const [futureDay, index] = stack[stack.length - 1];
+
+      if (futureDay <= temperatures[i]) {
+        stack.pop();
+      } else {
+        // found a warmer day
+        answer[i] = index - i;
+        stack.push([temperatures[i], i]);
+        break;
+      }
+    }
+
+    // if stack is empty, there is no future date with a warmer temp
+    if (!stack.length) {
+      answer[i] = 0;
+      stack.push([temperatures[i], i]);
+    }
+  }
+
+  return answer;
+};
+// -----------------------------------------------------------
+
 /**
  * Brute Force Solution: Nested Loops
  *
@@ -11,42 +43,42 @@
  */
 // Time: O(n^2)
 // Space: O(n)
-const dailyTemperatures = (temperatures) => {
-  if (!temperatures.length) return [];
-  const answer = [];
-  // set last value in answer to 0, since there are no future days
-  answer[temperatures.length - 1] = 0;
+// const dailyTemperatures = (temperatures) => {
+//   if (!temperatures.length) return [];
+//   const answer = [];
+//   // set last value in answer to 0, since there are no future days
+//   answer[temperatures.length - 1] = 0;
 
-  // iterate up until the second to last value in temperatures arr
-  for (let i = 0; i < temperatures.length - 1; i++) {
-    const currentDay = temperatures[i];
-    let count = 0; // value to track how many days until warmer day
+//   // iterate up until the second to last value in temperatures arr
+//   for (let i = 0; i < temperatures.length - 1; i++) {
+//     const currentDay = temperatures[i];
+//     let count = 0; // value to track how many days until warmer day
 
-    for (let j = i + 1; j < temperatures.length; j++) {
-      const futureDay = temperatures[j];
+//     for (let j = i + 1; j < temperatures.length; j++) {
+//       const futureDay = temperatures[j];
 
-      if (j === temperatures.length - 1 && futureDay <= currentDay) {
-        answer[i] = 0;
-        break;
-      }
+//       if (j === temperatures.length - 1 && futureDay <= currentDay) {
+//         answer[i] = 0;
+//         break;
+//       }
 
-      if (futureDay > currentDay) {
-        count++;
-        answer[i] = count;
-        count = 0;
-        break;
-      } else {
-        count++;
-      }
-    }
-  }
+//       if (futureDay > currentDay) {
+//         count++;
+//         answer[i] = count;
+//         count = 0;
+//         break;
+//       } else {
+//         count++;
+//       }
+//     }
+//   }
 
-  return answer;
-};
+//   return answer;
+// };
 
 console.log(dailyTemperatures([34, 80, 80, 80, 34, 80, 80, 80, 34, 34])); // [1,0,0,0,1,0,0,0,0,0]
-// console.log(dailyTemperatures([73, 74, 75, 71, 69, 72, 76, 73])); // [1,1,4,2,1,1,0,0]
-// console.log(dailyTemperatures([30, 40, 50, 60])); // [1,1,1,0]
-// console.log(dailyTemperatures([30, 60, 90])); // [1,1,0]
-// console.log(dailyTemperatures([3])); // [0]
-// console.log(dailyTemperatures([])); // []
+console.log(dailyTemperatures([73, 74, 75, 71, 69, 72, 76, 73])); // [1,1,4,2,1,1,0,0]
+console.log(dailyTemperatures([30, 40, 50, 60])); // [1,1,1,0]
+console.log(dailyTemperatures([30, 60, 90])); // [1,1,0]
+console.log(dailyTemperatures([3])); // [0]
+console.log(dailyTemperatures([])); // []
